da2e1bf19ecef42485df6968e6ca8235362b3dda
Ethan <epgreig@gmail.com>
2019-03-27 18:34:03 -0400

    beginning to build project in Xcode

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  euchre-engine
+//
+//  Created by Ethan Greig on 3/26/19.
+//  Copyright © 2019 Ethan Greig. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
