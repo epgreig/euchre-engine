8498df2c89b3bc29cc79c8c174591bc294528852
Ethan <epgreig@gmail.com>
2020-10-25 00:19:01 -0400

    building Scenario.cs and Caller.cs classes

---
+++ euchre.NET/Caller.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace euchre.NET
+{
+    public class Caller
+    {
+        public bool Declare(IEnumerable<Card> hand, Card upcard, int seat, bool firstRound, Random rng, out char? trump)
+        {
+            trump = null;
+
+            if (rng == null)
+                rng = new Random();
+
+            if (firstRound)
+            {
+                var score = ScoreHand(hand, upcard.Suit, upcard, seat);
+
+                if (seat == 1)
+                {
+                    var scores = new Dictionary<char, decimal>();
+                    foreach (char suit in Constants.SUITS)
+                    {
+                        if (suit != upcard.Suit && ScoreHand(hand, suit, upcard, 1) >= score)
+                            return false;
+                    }
+                }
+
+                var fraction = rng.NextDouble();
+                var interval = Constants.THRESHOLD['call'] - Constants.THRESHOLD['pass']
+                var threshold = Constants.THRESHOLD['pass'] + fraction * interval;
+
+                if (score > threshold)
+                {
+                    trump = upcard.Suit;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                decimal bestScore = 0;
+                char bestSuit = 'Z';
+                foreach (char suit in Constants.SUITS)
+                {
+                    if (suit != upcard.Suit)
+                    {
+                        var score = ScoreHand(hand, suit, upcard, seat);
+                        if (score > bestScore)
+                        {
+                            bestScore = score;
+                            bestSuit = suit;
+                        }
+                    }
+
+                }
+
+                var fraction = rng.NextDouble();
+                var interval = Constants.THRESHOLD['call'] - Constants.THRESHOLD['pass']
+                var threshold = Constants.THRESHOLD['pass'] + fraction * interval;
+
+                if (bestScore > threshold)
+                {
+                    trump = bestSuit;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        public decimal ScoreHand(IEnumerable<Card> hand, char suit, Card Upcard, int seat)
+        {
+
+        }
+    }
+}
--- euchre.NET/Scenario.cs
+++ euchre.NET/Scenario.cs
@@ -3,8 +3,62 @@ namespace euchre.NET
 {
     public class Scenario
     {
+        public Deal Deal;
+        public Random RNG;
+        public string Encoded;
+
         public Scenario()
         {
+            Deal = new Deal();
+            RNG = new Random();
+            ExecuteBiddingRound();
+        }
+
+        public Scenario(Deal deal)
+        {
+            Deal = deal;
+            RNG = new Random();
+            ExecuteBiddingRound();
+        }
+
+        public Scenario(Deal deal, int seed)
+        {
+            Deal = deal;
+            RNG = new Random(seed);
+            ExecuteBiddingRound();
+        }
+
+        public Scenario(string encoded)
+            => Decode(encoded);
+
+        private void ExecuteBiddingRound()
+        {
+            DetermineBid();
+            TrumpifyDeck();
+            Encode();
+        }
+
+        private void DetermineBid()
+        {
+            var pickup = true;
+            if (Caller.Declare(Deal.Hands[0], Deal.Upcard, 1, 1, RNG, out char trump))
+
+
+        }
+
+        private void TrumpifyDeck()
+        {
+
+        }
+
+        private void Encode()
+        {
+
+        }
+
+        private void Decode(string encoded)
+        {
+
         }
     }
 }
